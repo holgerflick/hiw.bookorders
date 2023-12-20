4faba5b4f68061680a1c97c05bf58f19042188e6
holger <holger@flixengineering.com>
2023-12-20 13:05:45 -0500

    created VCL project

---
+++ vcl/src/BookOrdersWin.dpr
@@ -0,0 +1,14 @@
+﻿program BookOrdersWin;
+
+uses
+  Vcl.Forms,
+  UFrmMain in 'UFrmMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
